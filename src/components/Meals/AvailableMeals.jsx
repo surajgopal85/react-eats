3753647cc5262b624360dd47a912141a33c9ebdc
Suraj Gopal <surajcancode@gmail.com>
2023-08-09 14:00:34 -0400

    touched up form component, styled items further, added card/input UI components

--- src/components/Meals/AvailableMeals.jsx
+++ src/components/Meals/AvailableMeals.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 
+// components
+import Card from "../UI/Card";
+import MealItem from "./MealItem";
+
+// style
 import classes from "./AvailableMeals.module.css";
 
 const DUMMY_MEALS = [
@@ -30,11 +35,21 @@ const DUMMY_MEALS = [
 ];
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => <li>{meal.name}</li>);
+  const mealsList = DUMMY_MEALS.map((meal) => (
+    <MealItem
+      id={meal.id}
+      key={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
 
   return (
     <section className={classes.meals}>
-      <ul>{mealsList}</ul>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
     </section>
   );
 };
