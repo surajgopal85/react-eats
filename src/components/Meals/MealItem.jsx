541fb7b0c9e57467c5e37781441f0d9105c57446
Suraj Gopal <surajcancode@gmail.com>
2023-08-11 15:14:58 -0400

    target achieved - introduced adding/removing meals through useContext in the cart tree. set up store folder to handle context, with usereducer to negotiate add/remove items. added animation to current cart button, ensured price updates follow add/remove items

--- src/components/Meals/MealItem.jsx
+++ src/components/Meals/MealItem.jsx
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 // style
 import classes from './MealItem.module.css';
 
 // components
 import MealItemForm from './MealItemForm';
+import CartContext from '../../store/cart-context';
 
 const MealItem = (props) => {
+  const cartCtx = useContext(CartContext);
   const price = `$${props.price.toFixed(2)}`;
+
+  const addToCartHandler = (amount) => {
+    cartCtx.addItem({
+      id: props.id,
+      name: props.name,
+      amount: amount,
+      price: props.price
+    });
+  }
 
   return (
     <li className={classes.meal}>
@@ -17,7 +28,7 @@ const MealItem = (props) => {
         <div className={classes.price}>{price}</div>
         </div>
       <div>
-        <MealItemForm id={props.id}/>
+        <MealItemForm onAddToCart={addToCartHandler} id={props.id}/>
       </div>
     </li>
   )
--- src/components/Meals/MealItemForm.jsx
+++ src/components/Meals/MealItemForm.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 
 // components
 import Input from "../UI/Input";
@@ -6,17 +6,40 @@ import Input from "../UI/Input";
 import classes from "./MealItemForm.module.css";
 
 const MealItemForm = (props) => {
+  const [amountIsValid, setAmountIsValid] = useState(true);
+  const amountInputRef = useRef();
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    const enteredAmount = amountInputRef.current.value;
+    const enteredAmountNum = +enteredAmount;
+    // enteredAmountNum.trim().length === 0 ||
+    if (enteredAmountNum < 1 || enteredAmountNum > 5) {
+      setAmountIsValid(false);
+      return;
+    }
+
+    // if arrive here, valid user entry
+    props.onAddToCart(enteredAmountNum);
+  };
+
   return (
-    <form className={classes.form}>
-      <Input label="Amount" input={{
-        id: 'amount' + props.id,
-        type: 'number',
-        min: '1',
-        max: '5',
-        step: '1',
-        defaultValue: '1'
-      }}/>
+    <form className={classes.form} onSubmit={submitHandler}>
+      <Input
+        ref={amountInputRef}
+        label="Amount"
+        input={{
+          id: "amount" + props.id,
+          type: "number",
+          min: "1",
+          max: "5",
+          step: "1",
+          defaultValue: "1",
+        }}
+      />
       <button>+ Add</button>
+      {!amountIsValid && <p>Please enter a valid amount (1-5).</p>}
     </form>
   );
 };
